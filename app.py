680c4092840a517b3e916ddd6f462ef93bf87914
Rajdeep Agrawal <55184929+raj26000@users.noreply.github.com>
2024-02-25 18:34:37 +0530

    FastAPI deployment using vercel

---
+++ app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, status, UploadFile
+import uvicorn
+import json
+import torch
+from transformers import AutoTokenizer, AutoModel
+from models import DiscourseEffectivenessModel
+from infer import inference
+
+app = FastAPI()
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+config['device'] = torch.device('cpu')
+tokenizer = AutoTokenizer.from_pretrained(config['pretrained_model'])
+# model = AutoModel.from_pretrained(config['saved_model_checkpoint']).to(config['device'])
+
+model = DiscourseEffectivenessModel().to(config['device'])
+model.load_state_dict(torch.load(config['saved_model_checkpoint'], map_location=config['device']))
+
+
+@app.post('/evaluateDiscourse', status_code=status.HTTP_200_OK)
+def predict_discourse_effectiveness(essay_file: UploadFile, discourse_type: str, discourse_text: str):
+    essay_text = essay_file.file.read()
+    probs = inference(tokenizer, discourse_type, discourse_text, str(essay_text), model)
+    print(probs)
+    return {"discourse_type": discourse_type,
+            "discourse_text": discourse_text,
+            "essay_text": str(essay_text),
+            "discourse_effectiveness_scores": {"Effective": round(probs[0][0].item(), 2),
+                                               "Adequate": round(probs[0][1].item(), 2),
+                                               "Ineffective": round(probs[0][2].item(), 2)
+                                               }
+            }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config['app_host'], port=config['app_port'])
